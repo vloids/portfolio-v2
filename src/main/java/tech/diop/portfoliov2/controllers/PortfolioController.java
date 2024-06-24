200cd84dec008453a358bce41d1648edb8a539d0
vloids <vloids@mail.com>
2024-06-24 20:16:12 +0200

    about me page: added mapping to PortfolioController.java, aminata-diop-cv.pdf can be downloaded from cv page, filler.jpg will be changed eventually, style.css updated for about.html page

--- src/main/java/tech/diop/portfoliov2/controllers/PortfolioController.java
+++ src/main/java/tech/diop/portfoliov2/controllers/PortfolioController.java
@@ -10,4 +10,9 @@ public class PortfolioController {
     public String home() {
       return "home";
     }
+
+    @GetMapping({ "/about"})
+    public String about() {
+        return "about";
+    }
 }
