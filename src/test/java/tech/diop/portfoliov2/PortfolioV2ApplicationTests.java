b6419d3561b70496d21a2561ba0659922f41a008
vloids <vloids@mail.com>
2024-06-24 13:21:05 +0200

    creation of project

---
+++ src/test/java/tech/diop/portfoliov2/PortfolioV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.diop.portfoliov2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
